a5d9950e01c40795c4e1bf464272c6b586af29c9
rodgco <r@rodg.co>
2019-05-08 08:24:52 -0300

    Completely replaced Virtus by self solution (see lib\telbe\helper.rb)

--- lib/telbe/location.rb
+++ lib/telbe/location.rb
@@ -1,16 +1,9 @@
 module Telbe
   class Location
-    include Virtus.model
-    attribute :longitude, Float
-    attribute :latitude, Float
+    include InitializeFromHash
   end
 
   class Venue
-    include Virtus.model
-    attribute :location, Location
-    attribute :address, String
-    attribute :title, String
-    attribute :foursquare_id, String
-    attribute :foursquare_type, String
+    include InitializeFromHash
   end
 end
