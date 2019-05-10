eb2969f37a7d5687b6cc493f8fb13c00554d9412
rodgco <r@rodg.co>
2019-05-10 08:49:51 -0300

    Improved MessageDescriptor & Helper functions

--- lib/telbe/bot.rb
+++ lib/telbe/bot.rb
@@ -54,8 +54,42 @@ module Telbe
     include InitializeFromHash
   end
 
+  # chat_id 	Integer or String 	Yes 	Unique identifier for the target chat or username of the target channel (in the format @channelusername)
+  # text 	String 	Yes 	Text of the message to be sent
+  # parse_mode 	String 	Optional 	Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in your bot's message.
+  # disable_web_page_preview 	Boolean 	Optional 	Disables link previews for links in this message
+  # disable_notification 	Boolean 	Optional 	Sends the message silently. Users will receive a notification with no sound.
+  # reply_to_message_id 	Integer 	Optional 	If the message is a reply, ID of the original message
+  # reply_markup 	InlineKeyboardMarkup or ReplyKeyboardMarkup or ReplyKeyboardRemove or ForceReply 	Optional 	Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
   class MessageDescriptor
     include InitializeFromHash
+    def self.factory
+      new(
+        chat_id: nil,
+        text: "",
+        parse_mode: "", # Markdown or HTML
+        disable_web_page_preview: false,
+        disable_notification: false,
+        reply_to_message_id: nil,
+        reply_markup: nil
+      )
+    end
+
+    def initialize(opts)
+      raise ArgumentError, 'Invalid parse_mode' unless valid_parse_mode? opts[:parse_mode]
+      super opts
+    end
+
+    def parse_mode= (value)
+      value ||= "" # cast nil to the empty string
+      raise ArgumentError, 'Invalid parse_mode' unless valid_parse_mode? value
+      @parse_mode = value
+    end
+
+    private
+    def valid_parse_mode? (value)
+      value =~ /(^Markdown$)|(^HTML$)|(^$)/ # 0 = true
+    end
   end
 
   class ChatPhoto
